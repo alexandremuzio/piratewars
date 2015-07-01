ed92fb52559d0939ccca508876392ebaa8fb8103
Matheus Camargo <matheuscamargo@gmail.com>
2015-07-01 16:54:41 -0300

    Added lobby comms logic to server

--- server/core/client.js
+++ server/core/client.js
@@ -9,6 +9,9 @@ var GameEngine = require('../../shared/game_engine.js');
 function Client(socket, room) {
 	// console.log("inside client constr");
 	this._id =  UUID();
+	this.name = null;
+	this.chosenTeam = null;
+	this.ready = false;
 	this._room = room;
 	this._socket = socket;
 	this._player = null;
@@ -21,46 +24,86 @@ function Client(socket, room) {
 Client.prototype.init = function() {
 	// console.log("client init");
 	this._socket.emit('onconnected');
-	this._socket.on('player.ready', this.onReady.bind(this));
+	this._socket.on('client.name', this.onName.bind(this));
+	this._socket.on('client.changeTeam', this.onChangeTeam.bind(this));
+	this._socket.on('client.ready', this.onReady.bind(this));
 	// process.on("SIGINT", function(){
  //    	console.log(this._packagesLost + " from " + this._lastStep + " lost (" + 100*this._packagesLost/this._lastStep + "%)");
 	// }.bind(this));
 }
 
-Client.prototype.onReady = function() {
-	// console.log("client onReady");
-	this._player = this.createPlayer();
-	console.log("after createPlayer");
-	this._socket.on('client.sync', this.queueSyncFromClient.bind(this));
-	this._room.clients.push(this);
+/*******************************************************/
+/****************** NETWORK CALLBACKS ******************/
+/*******************************************************/
+Client.prototype.onName = function(name) {
+	if (_.isString(name)) {
+		this.name = name;
+		this.chosenTeam =  this._room.getWeakestChosenTeam();
+		this._room.clients.push(this);
+	}
+	else {
+		console.error("Didn't get a string as name from the client");		
+	}
 }
 
+Client.prototype.onChangeTeam = function(team) {
+	if (this._room.validateTeam(team)) {
+		this.chosenTeam = team;
+	}
+	else {
+		console.error("Invalid chosen team");		
+	}
+}
+
+Client.prototype.onReady = function(ready) {
+	if (_.isBoolean(ready)) {
+		this.ready = ready;
+	}
+	else {
+		console.error("Invalid ready state");		
+	}
+}
+
+
+/******************************************************/
+/****************** HELPER FUNCTIONS ******************/
+/******************************************************/
 Client.prototype.createPlayer = function() {
 	// console.log("client createPlayer");
-	var entity = PlayerFactory.createPlayer(this._socket, this._snapshots);
+	this._player = PlayerFactory.createPlayer(this._socket, this._snapshots);
 	this._socket.emit('player.create', 
 		{
 			id: entity.id,
 			transform: entity.transform.getPosition(),
 			initialAttrs: entity.initialAttrs.getAll()
 		});
-	return entity;
+	this._socket.on('client.sync', this.queueSyncFromClient.bind(this));
 }
 
 Client.prototype.queueSyncFromClient = function(message) {
+	// Debug package loss
 	if (message.step !== this._lastStep + 1) {
 		this._packagesLost += (this._lastStep + 1) - message.step; 
 	}
 	this._lastStep = message.step;
+
 	this._snapshots.add(message);
 }
 
+
+/****************************************************/
+/****************** SYNC FUNCTIONS ******************/
+/****************************************************/
 Client.prototype.sendGameSync = function(snapshot) {
 	this._socket.emit('game.sync', snapshot);
 }
 
 Client.prototype.sendChangedState = function(newState) {
 	this._socket.emit('game.state', newState);
+}
+
+Client.prototype.sendLobbyInfo = function(info) {
+	this._socket.emit('lobby.info', info);
 }
 
 module.exports = Client;
